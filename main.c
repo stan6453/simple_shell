356d97009162f45eb439042a9b558b492e43a239
stanley <stanley6453@gmail.com>
2022-11-16 08:17:30 +0100

    added string_funcs.c

--- main.c
+++ main.c
@@ -148,54 +148,11 @@ void delete_char_at_index(char *str, int index)
 }
 
 
-/**
- * removecomment - unsets the value of an environment variable
- * @str: name of the environment variable to unset
- */
-void removecomment(char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-		if (str[i] == '#')
-		{
-			str[i] = '\0';
-			return;
-		}
-}
 
 
 
-/**
- * tokenize_string - set the value of an environment variable
- * @command: name of env variable to set the value
- * @commandarray: value to be set
- * @sep: if set to true, environment variable is overwri
- */
-void tokenize_string(char *command, char *commandarray[], char sep)
-{
-	int i;
-	int count_ca = 0;
-	int pass_space = 1;
 
-	for (i = 0; command[i] != '\0'; i++)
-	{
-		if (pass_space == 1 && command[i] != sep)
-		{
-			commandarray[count_ca] = command + i;
-			count_ca++;
-			pass_space = 0;
-		}
-		else if (command[i] == sep && pass_space != 1)
-		{
-			command[i] = '\0';
-			pass_space = 1;
-		}
 
-	}
-
-	commandarray[count_ca] = NULL;
-}
 
 
 /**
@@ -496,23 +453,7 @@ int exit_command(char *commandarray[], char *env[])
 }
 
 
-/**
- * isonlydigit - checks if a string contains only digits
- * @s: string to be checked
- * Description: checks if a string contains only digit
- * Return: 1 if string contains only didgit
- *         0 if string has at lest one non-digit character
- */
-int isonlydigit(char *s)
-{
-	while (*s != '\0')
-	{
-		if (*s < '0' || *s > '9')
-			return (0);
-		s++;
-	}
-	return (1);
-}
+
 
 
 
@@ -681,142 +622,6 @@ char *resolve_path(char *myprog, char *progname, char *pathvar)
 	perror("");
 	return (NULL);
 }
-
-
-
-
-
-/**
- * _str_len - finds length of a string
- * @str: string to find its length
- * Description: finds length of a string (not including null character)
- * Return: length of string
- */
-int _str_len(const char *str)
-{
-	int i;
-
-	if (str == NULL)
-		return (0);
-
-	for (i = 0; str[i] != '\0'; i++)
-		;
-	return (i);
-}
-
-/**
- * comp_env_with_val - check if val matches the name of an environment variable
- * @env: string in the form "name=value"
- * @val: this string is compared with name from the string "name=valuee"
- * Return: 0 is the strings match, 1 if they don't
- */
-
-int comp_env_with_val(const char *env, const char *val)
-{
-	int i;
-
-	for (i = 0; ; i++)
-	{
-		if (env[i] != val[i])
-		{
-			if (env[i] == '=' && val[i] == '\0')
-				return (0);
-			else
-				return (1);
-		}
-	}
-}
-
-
-
-/**
- * _getenv - return a pointer to the value of an environment variable
- * @name: name of the environment variable to search for its value
- * Return: pointer to the value of an environment variable searched
- *         NULL if not found
- */
-char *_getenv(const char *name)
-{
-	/*extern char **environ;*/
-	int i;
-
-	for (i = 0; environ[i] != NULL; i++)
-	{
-		if (comp_env_with_val(environ[i], name) == 0)
-		{
-			return (environ[i] + _str_len(name) + 1);
-		}
-	}
-	return (NULL);
-}
-
-
-
-/**
- * _setenv - set the value of an environment variable
- * @name: name of env variable to set the value
- * @value: value to be set
- * @overwrite: if set to true, environment variable
- * Return: 0 on success, -1 on failure (with errno
- */
-
-int _setenv(const char *name, const char *value, int overwrite)
-{
-	int i, j;
-	/*extern char **environ;*/
-	int memsize;
-
-	for (i = 0; environ[i] != NULL; i++)
-	{
-		if (comp_env_with_val(environ[i], name) == 0)
-		{
-			if (overwrite)
-			{
-				memsize = _str_len(value) + _str_len(name) + 2;
-				environ[i] = malloc(memsize);
-				_strcpy(environ[i], name);
-				_strcat(environ[i], "=");
-				_strcat(environ[i], value);
-			}
-			return (0);
-		}
-	}
-
-	/*
-	 * add a new environment variable
-	 */
-}
-
-
-
-
-/**
- * _unsetenv - unsets the value of an environment variable
- * @name: name of the environment variable to unset
- * Return: 0 in success, -1 on error (with errno set (not implemented yet))
- */
-int _unsetenv(const char *name)
-{
-	/*extern char **environ;*/
-	int i;
-
-	for (i = 0; environ[i] != NULL; i++)
-	{
-		if (comp_env_with_val(environ[i], name) == 0)
-		{
-			/*
-			* delete from array
-			*/
-			for (; environ[i] != NULL; i++)
-			{
-				environ[i] = environ[i + 1];
-			}
-			return (0);
-		}
-	}
-	return (0);
-}
-
 
 
 
